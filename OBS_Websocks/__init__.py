e1ed929370ec5961ed1dab8b82592cd58ea54b0c
Aaron <23297034+Aaronsss@users.noreply.github.com>
2023-10-29 17:35:43 +0000

    Updated to work with RH 4.0.0

--- OBS_Websocks/__init__.py
+++ OBS_Websocks/__init__.py
@@ -1,4 +1,4 @@
-'''OBS Interface'''
+'''OBS Actions'''
 
 OBS_IP = 'localhost'
 OBS_Port = '4455'
@@ -8,17 +8,14 @@ import logging
 logger = logging.getLogger(__name__)
 from eventmanager import Evt
 from EventActions import ActionEffect
+from RHUI import UIField, UIFieldType, UIFieldSelectOption
 import simpleobsws
 import asyncio
 import asyncio_gevent
 
 asyncio.set_event_loop_policy(asyncio_gevent.EventLoopPolicy())
 loop = asyncio.get_event_loop()
-
-ws = simpleobsws.WebSocketClient(url = 'ws://' + OBS_IP + ':' + OBS_Port, password = OBS_Password) 
-
-def connectToOBS(args):
-    loop.run_until_complete(OBSConnect())
+ws = simpleobsws.WebSocketClient(url = 'ws://' + OBS_IP + ':' + OBS_Port, password = OBS_Password)
 
 async def OBSConnect():
     try:
@@ -30,9 +27,6 @@ async def OBSConnect():
             print("*** OBS failed to connect ***")
     except:
         print("*** OBS Server is not active ***")
-
-def disconnectFromOBS(args):
-    loop.run_until_complete(OBSDisconnect())
 
 async def OBSDisconnect():
     try:
@@ -51,66 +45,61 @@ async def OBSRecord(recMode):
 async def OBSScene(Scene_Select):
     if Scene_Select != '':
         await ws.emit(simpleobsws.Request('SetCurrentProgramScene', { 'sceneName': Scene_Select }))
-    
-def registerHandlers(args):
-    if 'registerFn' in args:
-        for effect in obsdiscover():
-            args['registerFn'](effect)
 
-def initialize(**kwargs):
-    if 'Events' in kwargs:
-        kwargs['Events'].on(Evt.STARTUP, 'OBS_Websocks_Interface', connectToOBS, {}, 75)
-        kwargs['Events'].on(Evt.SHUTDOWN, 'OBS_Websocks_Interface', disconnectFromOBS, {}, 75)
-        kwargs['Events'].on('actionsInitialize', 'OBS_Websocks_Interface', registerHandlers, {}, 120, True)
-        
-def obsMessageEffect(action, args):
+class OBS_Actions():
+    def __init__(self, rhapi):
+        self._rhapi = rhapi
 
-    try:
-        OBS_scene = action['scene']
-    except:
-        OBS_scene = ''
-    try:
-        OBS_record = action['record']
-    except:
-        OBS_record = 0
-    try:
-        OBS_Connect = action['connect']
-    except:
-        OBS_Connect = 0
-
-    is_identified = simpleobsws.WebSocketClient.is_identified(ws)
-
-    if (OBS_Connect != '0' and OBS_Connect != '') and not is_identified:
-        print("OBS trying to connect...")
+    def connectToOBS(self, args):
         loop.run_until_complete(OBSConnect())
 
-    if is_identified:
-        loop.run_until_complete(OBSScene(OBS_scene))
-        loop.run_until_complete(OBSRecord(OBS_record))
-        logger.debug("OBS scene changed to: {}, recording: {}". format(OBS_scene, OBS_record))
+    def disconnectFromOBS(self, args):
+        loop.run_until_complete(OBSDisconnect())
+    
+    def obsMessageEffect(self, action, args):
 
-def obsdiscover():
-    return [
-        ActionEffect(
-            'obsmessage',
-            'OBS Message',
-            obsMessageEffect,
-            [
-                {
-                    'id': 'scene',
-                    'name': 'OBS Scene',
-                    'type': 'text',
-                },
-                {
-                    'id': 'record',
-                    'name': 'OBS Record (0 - no change, 1 - record, 2 - stop)',
-                    'type': 'text',
-                },                
-                {
-                    'id': 'connect',
-                    'name': 'OBS Connect (Try to connet to OBS)',
-                    'type': 'text',
-                }
-            ]
-        )
-    ]
+        try:
+            OBS_scene = action['scene']
+        except:
+            OBS_scene = ''
+        try:
+            OBS_record = action['record']
+        except:
+            OBS_record = 0
+        try:
+            OBS_Connect = action['connect']
+        except:
+            OBS_Connect = 0
+
+        is_identified = simpleobsws.WebSocketClient.is_identified(ws)
+
+        if (OBS_Connect != '0' and OBS_Connect != '') and not is_identified:
+            print("OBS trying to connect...")
+            loop.run_until_complete(OBSConnect())
+
+        if is_identified:
+            loop.run_until_complete(OBSScene(OBS_scene))
+            loop.run_until_complete(OBSRecord(OBS_record))
+            logger.debug("OBS scene changed to: {}, recording: {}". format(OBS_scene, OBS_record))
+
+    def register_handlers(self, args):
+        if 'register_fn' in args:
+            for effect in [
+                ActionEffect(
+                    'OBS Action',
+                    self.obsMessageEffect,
+                    [
+                        UIField('scene', "OBS Scene", UIFieldType.TEXT),
+                        UIField('record', "OBS Record (0 - no change, 1 - record, 2 - stop)", UIFieldType.TEXT),
+                        UIField('connect', "OBS Connect (Try to connet to OBS)", UIFieldType.TEXT)
+                    ]
+                )
+            ]:
+                args['register_fn'](effect)
+
+def initialize(rhapi):
+    obs = OBS_Actions(rhapi)
+    rhapi.events.on(Evt.STARTUP, obs.connectToOBS)
+    rhapi.events.on(Evt.SHUTDOWN, obs.disconnectFromOBS)
+    rhapi.events.on(Evt.ACTIONS_INITIALIZE, obs.register_handlers)
+  
